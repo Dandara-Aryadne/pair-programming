d88325e51d42dbc982e2de2f30fee13a86dd1f64
Dandara-Aryadne <dandaraaryadne@hotmail.com>
2022-06-22 17:57:04 -0300

    inserindo a compenetização

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 export default function App(){
   return(
-    <><h1>oi</h1></>
+    <>
+    <h1>oi</h1>
+    < Header />
+    < Main />
+    <Footer />
+    </>
   )
 }
